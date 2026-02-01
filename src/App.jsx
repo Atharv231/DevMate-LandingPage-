deb47a6b3526c45b2da2d7a413ea1baddfe28a10
Atharva Ahire <atharvahire2006@gmail.com>
2026-02-01 10:41:07 +0530

    Adding new files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Landing from "./pages/Landing";
+import Demo2 from "./pages/Demo2";
+import Second from "./pages/Second";
+import CommunicationSection from "./pages/CommunicationSection";
+import RealtimeSection from "./pages/RealtimeSection";
+import Experience from "./pages/ExperienceSection";
+
+function App() {
+  return (
+    <>
+      <Landing />
+      {/* <Demo2 />
+      <Second />
+      <CommunicationSection /> */}
+      {/* <RealtimeSection /> */}
+      {/* <Experience /> */}
+    </>
+  );
+}
+
+export default App;
